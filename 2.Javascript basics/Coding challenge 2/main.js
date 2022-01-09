67a8d672ecffd210ace696d498296c326fbc1d23
Malik-Lemesh <malik.lemesh@gmail.com>
2022-01-09 13:20:13 +0100

    coding challenge 2 finish

--- 2.Javascript basics/Coding challenge 2/main.js
+++ 2.Javascript basics/Coding challenge 2/main.js
@@ -1,8 +1,32 @@
-var scoreTeamJohn = (89 + 120 + 103) / 3;
+var scoreTeamJohn = (89 + 135 + 103) / 3;
 var scoreTeamMike = (116 + 94 + 123) / 3;
+var scoreTeamMary = (97 + 134 + 105) / 3;
+console.log(scoreTeamJohn, scoreTeamMike, scoreTeamMary);
 
-if (scoreTeamJohn > scoreTeamMike) {
-  console.log("John's team win with average score " + scoreTeamJohn);
-} else if (scoreTeamJohn < scoreTeamMike) {
-  console.log("Mike's team win with average score " + scoreTeamMike);
+if (scoreTeamJohn > scoreTeamMike && scoreTeamJohn > scoreTeamMary) {
+  console.log(
+    "John's team win with average score " + scoreTeamJohn + " points."
+  );
+} else if (scoreTeamMike > scoreTeamJohn && scoreTeamMike > scoreTeamMary) {
+  console.log(
+    "Mike's team win with average score " + scoreTeamMike + " points."
+  );
+} else if (scoreTeamMary > scoreTeamJohn && scoreTeamMary > scoreTeamMike) {
+  console.log(
+    "Mary's team win with average score " + scoreTeamMary + " points."
+  );
+} else {
+  console.log("There is a draw.");
 }
+
+// if (scoreTeamJohn > scoreTeamMike) {
+//   console.log(
+//     "John's team win with average score " + scoreTeamJohn + " points."
+//   );
+// } else if (scoreTeamJohn < scoreTeamMike) {
+//   console.log(
+//     "Mike's team win with average score " + scoreTeamMike + " points."
+//   );
+// } else {
+//   console.log("There is a draw.");
+// }
