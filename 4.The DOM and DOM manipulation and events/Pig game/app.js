05b0b42df422f0f85f798788a84e5752f645a6ea
Malik-Lemesh <malik.lemesh@gmail.com>
2022-03-01 23:03:53 +0100

    deveti cas

--- 4.The DOM and DOM manipulation and events/Pig game/app.js
+++ 4.The DOM and DOM manipulation and events/Pig game/app.js
@@ -61,18 +61,46 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     document.querySelector("#current-" + activePlayer).textContent = roundScore;
   } else {
     //Next player
-    //if                 uradi ovo         else
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-    roundScore = 0;
-    //Postavlja vrijednosti na 0 kod oba playera
-    document.getElementById("current-0").textContent = "0";
-    document.getElementById("current-1").textContent = "0";
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-    // document.querySelector(".player-0-panel").classList.remove("active");
-    // document.querySelector(".player-1-panel").classList.add("active");
-    document.querySelector(".dice").style.display = "none";
+    nextPlayer();
   }
 });
+
+//selektuje odredenu klasu                         //anonimus funkcija
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  //Add CURRENT score to GLOBAL score
+  scores[activePlayer] += roundScore;
+
+  //update the UI
+  document.querySelector("#score-" + activePlayer).textContent =
+    scores[activePlayer];
+
+  //check if player won the game
+  if (scores[activePlayer] >= 10) {
+    document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+    document.querySelector(".dice").style.display = "none";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+  } else {
+    //next player
+    nextPlayer();
+  }
+});
+
+function nextPlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  roundScore = 0;
+  //Postavlja vrijednosti na 0 kod oba playera
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  //selektuje odredenu klasu
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  // document.querySelector(".player-0-panel").classList.remove("active");
+  // document.querySelector(".player-1-panel").classList.add("active");
+  document.querySelector(".dice").style.display = "none";
+}
